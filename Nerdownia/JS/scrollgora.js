8c613acf48cc03a8b2d03a92f341ac72d292182d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-07-07 18:26:36 +0200

    Update

--- Nerdownia/JS/scrollgora.js
+++ Nerdownia/JS/scrollgora.js
@@ -1,12 +1,9 @@
-﻿jQuery(function($)
-{
+﻿jQuery(function ($) {
 	$.scrollTo(0);
-	$('.scrollgora').click(function() { $.scrollTo($('.strona'), 1000); });
-}
-);
-$(window).scroll(function()
-{
-	if($(this).scrollTop()>0) $('.scrollgora').fadeIn();
-	else $('.scrollgora').fadeOut();		
-}
-);
+	$('.scrollgora').click(function () { $.scrollTo($('.strona'), 1000); });
+});
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 0) $('.scrollgora').fadeIn();
+	else $('.scrollgora').fadeOut();
+});
